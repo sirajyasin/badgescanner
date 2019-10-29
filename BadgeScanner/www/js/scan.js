674f27fdd00a54c51b688f3ff996cab2ec1c8a61
Siraj <sy495p@att.com>
2019-10-29 10:55:38 -0500

    Badge scanner

--- BadgeScanner/www/js/scan.js
+++ BadgeScanner/www/js/scan.js
@@ -2,12 +2,12 @@ function startScan() {
         var valid_users = ['efgjkmp', 'edihyav', 'eabimod', 'ebalkan', 'eamaral', 'efeomaz']
 	cordova.plugins.barcodeScanner.scan(
 		function (result) {
-		  alert(result.text);
-		  alert('check and take some action');
 		  if (valid_users.includes(result.text)) {
+			  alert('valid user, gate open');
 			  $('#blink')[0].style.display = 'block';
 			  $('#blink')[0].style.background = 'green';
 		  } else {
+			  alert('Invalid user, Please try again');
 			  $('#blink')[0].style.display = 'block';
 			  $('#blink')[0].style.background = 'red';
 	          }
