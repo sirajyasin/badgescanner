ffddec8a2a5b0a2d76f677c80650569314d9af67
Siraj <sy495p@att.com>
2019-10-29 09:54:43 -0500

    Few fixes

--- BadgeScanner/www/js/scan.js
+++ BadgeScanner/www/js/scan.js
@@ -2,14 +2,12 @@ function startScan() {
         var valid_users = ['efgjkmp', 'edihyav', 'eabimod', 'ebalkan', 'eamaral', 'efeomaz']
 	cordova.plugins.barcodeScanner.scan(
 		function (result) {
-		  alert(result)
-		  alert('check and take some action')
+		  alert(result);
+		  alert('check and take some action');
 		  if (valid_users.include(result.text)) {
-			  document.body.style.background = "green"; 
 			  $('#blink').style.display = 'block';
 			  $('#blink').style.background = 'green';
 		  } else {
-			  document.body.style.background = "red"; 
 			  $('#blink').style.display = 'block';
 			  $('#blink').style.background = 'red';
 	          }
