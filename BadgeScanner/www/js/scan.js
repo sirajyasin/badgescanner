86734def0b242765b1d60ffad01c2b6990c18d31
Siraj <sy495p@att.com>
2019-10-29 09:46:27 -0500

    Updated with some Js fix

--- BadgeScanner/www/js/scan.js
+++ BadgeScanner/www/js/scan.js
@@ -1,16 +1,17 @@
-document.addEventListener("deviceready", init, false);
-function init() {
-	document.querySelector("#startScan").addEventListener("touchend", startScan, false);
-}
-
 function startScan() {
         var valid_users = ['efgjkmp', 'edihyav', 'eabimod', 'ebalkan', 'eamaral', 'efeomaz']
 	cordova.plugins.barcodeScanner.scan(
 		function (result) {
+		  alert(result)
+		  alert('check and take some action')
 		  if (valid_users.include(result.text)) {
 			  document.body.style.background = "green"; 
+			  $('#blink').style.display = 'block';
+			  $('#blink').style.background = 'green';
 		  } else {
 			  document.body.style.background = "red"; 
+			  $('#blink').style.display = 'block';
+			  $('#blink').style.background = 'red';
 	          }
                         
 		}, 
