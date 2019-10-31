53dcd6d1c575ee7738028ba33e3358a0d5d3c0cb
Siraj <sy495p@att.com>
2019-10-30 20:45:24 -0500

    Added API server IP

--- BadgeScanner/www/js/scan.js
+++ BadgeScanner/www/js/scan.js
@@ -1,5 +1,5 @@
 function startScan() {
-        var valid_users = ['efgjkmp', 'edihyav', 'eabimod', 'ebalkan', 'eamaral', 'efeomaz']
+        var valid_users = ['efgjkmp', 'ebalkan', 'eamaral']
 	cordova.plugins.barcodeScanner.scan(
 		function (result) {
 		  if (valid_users.includes(result.text)) {
@@ -15,7 +15,20 @@ function startScan() {
 		}, 
 		function (error) {
 			  document.body.style.background = "orange"; 
-		}
+		},
+		{
+                  preferFrontCamera : true, // iOS and Android
+                  showFlipCameraButton : true, // iOS and Android
+                  showTorchButton : true, // iOS and Android
+                  torchOn: true, // Android, launch with the torch switched on (if available)
+                  saveHistory: true, // Android, save scan history (default false)
+                  prompt : "Place a barcode inside the scan area", // Android
+                  resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+                  formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+                  orientation : "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+                  disableAnimations : true, // iOS
+                  disableSuccessBeep: false // iOS and Android
+                }
 	);
 
 }
