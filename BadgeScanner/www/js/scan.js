df6e92328173666b8d4abc4ca478ac18baf81bd4
Siraj <sy495p@att.com>
2019-10-31 21:50:03 -0500

    Testing

--- BadgeScanner/www/js/scan.js
+++ BadgeScanner/www/js/scan.js
@@ -1,15 +1,51 @@
+
+var red_plug_id = "8006483CBBE44CAE81CCD5BC07526DF61A5A9B8E";
+var green_plug_id = "80065D728E14913AAD1899ABBD49774E1A5A1365";
+var token = "f1f2df00-A1v58z3AR3UJ8wcuwUI9Ks9";
+
+function api_call(state, device_id) {
+   var api_server = 'https://wap.tplinkcloud.com/';
+   var _data = {
+     "method": "passthrough",
+     "params": {
+       "deviceId": device_id,
+       "requestData": "{\"system\":{\"set_relay_state\":{\"state\":"+ state +"}}}",
+       "token": token
+     }
+   }
+  
+   $.support.cors = true;
+   $.ajax({
+      url:  api_server,
+      type: "POST",
+      data: JSON.stringify(_data),
+      dataType: "json",
+      contentType: "application/json; charset=utf-8"
+   });
+}
+
+function allow_user() {
+    api_call(1, green_plug_id);
+    setTimeout(function(){ api_call(0, green_plug_id); }, 1000);
+}
+
+function deny_user() {
+    api_call(1, red_plug_id);
+    setTimeout(function(){ api_call(0, red_plug_id); }, 1000);
+}
+
 function startScan() {
-        var valid_users = ['efgjkmp', 'ebalkan', 'eamaral']
+        var valid_users = ['efgjkmp', 'ebalkan', 'eamaral'];
 	cordova.plugins.barcodeScanner.scan(
 		function (result) {
 		  if (valid_users.includes(result.text)) {
-			  alert('valid user, gate open');
 			  $('#blink')[0].style.display = 'block';
 			  $('#blink')[0].style.background = 'green';
+			  allow_user();
 		  } else {
-			  alert('Invalid user, Please try again');
 			  $('#blink')[0].style.display = 'block';
 			  $('#blink')[0].style.background = 'red';
+			  deny_user();
 	          }
                         
 		}, 
@@ -32,3 +68,5 @@ function startScan() {
 	);
 
 }
+
+
