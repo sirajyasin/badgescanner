00585c2f5858dfd108edd6a80ee31ca906cf7d00
siraj.yasin <sirajudeen.yasin@gmail.com>
2019-11-11 22:12:02 +0000

    Testing from new VM

--- BadgeScanner/platforms/android/app/src/main/assets/www/js/scan.js
+++ BadgeScanner/platforms/android/app/src/main/assets/www/js/scan.js
@@ -1,7 +1,8 @@
 
 var red_plug_id = "8006483CBBE44CAE81CCD5BC07526DF61A5A9B8E";
 var green_plug_id = "80065D728E14913AAD1899ABBD49774E1A5A1365";
-var plug_token = "f1f2df00-A1v58z3AR3UJ8wcuwUI9Ks9";
+//var plug_token = "f1f2df00-A1v58z3AR3UJ8wcuwUI9Ks9";
+var plug_token = "f1f2df00-A3Uc43Ujs5w2Ov59RlccQF0";
 var light_token = "cb285a31cee4325fddb8fffddfd80dfb5c1d24cbb797598da8772c83b3453fb6";
 
 
@@ -64,6 +65,7 @@ function allow_user() {
     turn_on_green_light()
     setTimeout(function(){ api_call(0, green_plug_id); }, 500);
     setTimeout(function(){ turn_off_light(); }, 6000);
+    setTimeout(startScan, 10000);
     startScan();
 }
 
@@ -72,6 +74,7 @@ function deny_user() {
     turn_on_red_light()
     setTimeout(function(){ api_call(0, red_plug_id); }, 500);
     setTimeout(function(){ turn_off_light(); }, 6000);
+    setTimeout(startScan, 10000);
     startScan();
 }
 
@@ -85,19 +88,24 @@ function startScan() {
 	}
 	cordova.plugins.barcodeScanner.scan(
 		function (result) {
-		  if (valid_users.includes(result.text)) {
-			  $('#blink')[0].style.display = 'block';
-			  $('#blink')[0].style.background = 'green';
-			  allow_user();
-		  } else {
-			  $('#blink')[0].style.display = 'block';
-			  $('#blink')[0].style.background = 'red';
-			  deny_user();
-	          }
+		  var  scan_result = result.text;
+		  if ( scan_result && scan_result.length == 19 ) {
+		      var usr = scan_result.trim().substr(0,7);
+		      if (valid_users.includes(usr)) {
+		              $('#blink')[0].style.display = 'block';
+		              $('#blink')[0].style.background = 'green';
+		              allow_user();
+		      } else {
+		              $('#blink')[0].style.display = 'block';
+		              $('#blink')[0].style.background = 'red';
+		              deny_user();
+	              }
+		  }
                         
 		}, 
 		function (error) {
 			  document.body.style.background = "orange"; 
+			  setTimeout(startScan, 10000);
 		},
 		{
                   preferFrontCamera : true, // iOS and Android
